e353695cbd120599ae9a50f03008b96f820d284c
Jason Fox <jason@jasonfox.com>
2010-06-21 14:32:10 -0400

    Updating rake tasks for named queues

--- tasks/tasks.rb
+++ tasks/tasks.rb
@@ -5,11 +5,11 @@ task :merb_env
 namespace :jobs do
   desc "Clear the delayed_job queue."
   task :clear => [:merb_env, :environment] do
-    Delayed::Job.delete_all
+    ENV['QUEUE'] ? Delayed::Job.delete_all("queue = '#{ ENV['QUEUE'] }") : Delayed::Job.delete_all
   end
 
   desc "Start a delayed_job worker."
   task :work => [:merb_env, :environment] do
-    Delayed::Worker.new(:min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY']).start
+    Delayed::Worker.new(:min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY'], :queue => ENV['QUEUE']).start
   end
 end
